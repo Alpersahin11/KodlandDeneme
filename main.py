ad79ca1c67ca20103c8ee0303431a611f880bbc6
Alpersahin11 <159240555+Alpersahin11@users.noreply.github.com>
2024-02-28 22:03:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("kodland")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+    print(15,"giriş yaptı")
+    
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    if count_heh > 1000:
+        await ctx.send("beni tekerleme yapan adam mı sandın")
+    else:
+        await ctx.send("he"* count_heh)
+
+@bot.command()
+async def deniz(ctx,a):
+    await ctx.send("merhaba deniz" * int(a) )
+
+
+
+bot.run("")
